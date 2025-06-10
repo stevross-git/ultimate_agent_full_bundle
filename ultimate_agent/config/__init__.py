bdda1dcfb81fc68a319d1e4bd31a604de62f0997
Steve Davies <stephen@peoplesainetwork.com>
2025-06-10 14:02:44 +1000

    Fix import statement for config and update print statement to use correct variable

--- ultimate_agent/config/__init__.py
+++ ultimate_agent/config/__init__.py
@@ -1,5 +1,5 @@
 # ultimate_agent/config/__init__.py
 
-from .settings import config  # ✅ import the renamed variable
+from .settings import config  # ✅ correctly imported config dictionary
 
-print(settings['port'])  # ✅ This works
+print(config['port'])  # ✅ now this will work
